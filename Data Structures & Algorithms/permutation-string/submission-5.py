e346ff5da28d5fd1d5052bf35109e62b8040ba81
GawthamW97 <39323106+GawthamW97@users.noreply.github.com>
2026-04-16 10:40:23 +0100

    Add: permutation-string - submission-5

---
+++ Data Structures & Algorithms/permutation-string/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n1,n2 = len(s1), len(s2)
+        window = [0] * 26
+        need = [0] * 26
+
+        for char in s1:
+            need[ord(char) - ord("a")] +=1
+        
+        for char in s2[:n1]:
+            window[ord(char) - ord("a")] +=1
+        
+        if window == need:
+            return True
+        
+        for r in range(n1,n2):
+            window[ord(s2[r]) - ord("a")] +=1
+            window[ord(s2[r - n1]) - ord("a")] -=1
+            if window == need:
+                return True
+        return False
+         
+                
