5e20d1ae5b16338e34fa74be70ded7ef3f25ec9f
GawthamW97 <39323106+GawthamW97@users.noreply.github.com>
2026-09-13 13:34:37 +0100

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        #Map each course to its prerequisites
+        preMap = {i : [] for i in range(numCourses)}
+
+        for crs,pre in prerequisites:
+            preMap[crs].append(pre)
+
+        #store all courss along the current DFS path
+        visited = set()
+
+        def dfs(crs):
+            if crs in visited:
+                # cycle detected
+                return False
+            if preMap[crs] == []:
+                return True
+
+            visited.add(crs)
+
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visited.remove(crs)
+            preMap[crs] = []
+            
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True
